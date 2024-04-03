be74e04de243bbd85066fb93d7a87ad9d14b1885
evgenii <136128227+evgeniikruglikov@users.noreply.github.com>
2024-04-03 17:41:55 +0300

    Add files via upload

---
+++ Учебные материалы/rabota12/rabota12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+
+namespace zadanie12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // чтение слов из файла rus01.txt
+            string[] words = File.ReadAllLines("rus01.txt");
+
+            // сортируем слова в алфавитном порядке, потом переворачиваем
+            Array.Sort(words);
+            Array.Reverse(words);
+
+            // записываем отсортированные слова в rus02.txt
+            File.WriteAllLines("rus02.txt", words);
+        }
+    }
+}
